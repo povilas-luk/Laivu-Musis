aadcb031f1e91ccf6f2f907128ed180c388a2e62
50kw <50887365+50kw@users.noreply.github.com>
2019-05-22 09:43:18 +0300

    Add files via upload

---
+++ LaivuMusis/LaivuMusis/Bibliotekos.h
@@ -0,0 +1,16 @@
+#ifndef Bibliotekos_h
+#define Bibliotekos_h
+
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <Windows.h>
+#include "Laivai.h"
+#include "Lenta.h"
+#include "Zaidejas.h"
+#include "Zaisti.h"
+
+#endif	/*Bibliotekos_h*/
+
